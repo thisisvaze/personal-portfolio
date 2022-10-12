1a6f67d3307ba9eed6ced15f1627c71cceaec051
Aaditya Vaze <aadityavikasvaze@gmail.com>
2022-10-12 01:33:23 -0400

    finishing touches

--- .eleventy.js
+++ .eleventy.js
@@ -78,11 +78,48 @@ module.exports = function (eleventyConfig) {
           src="${lowestSrc.url}"
           width="${lowestSrc.width}"
           class="w-100  col-8"
+          loading="lazy"
+          decoding="async"
           alt="${alt}">
       </picture>`;
     }
   );
 
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "myProfilePhoto",
+    async function (src, alt) {
+      if (alt === undefined) {
+        // You bet we throw an error on missing alt (alt="" works okay)
+        throw new Error(`Missing \`alt\` on myResponsiveImage from: ${src}`);
+      }
+      let outputFormat = "webp";
+      let stats = await Image(src, {
+        widths: [1024],
+        formats: ["webp"],
+        urlPath: "/img/",
+        outputDir: "./dev/img/",
+      });
+      let lowestSrc = stats[outputFormat][0];
+      let sizes = "100vw"; // Make sure you customize this!
+      // Iterate over formats and widths
+      return `<picture>
+      ${Object.values(stats)
+        .map((imageFormat) => {
+          return `  <source type="image/${
+            imageFormat[0].format
+          }" srcset="${imageFormat
+            .map((entry) => `${entry.url} ${entry.width}w`)
+            .join(", ")}" sizes="${sizes}">`;
+        })
+        .join("\n")}
+        <img
+          src="${lowestSrc.url}"
+          width="${lowestSrc.width}"
+          style="height:15vh; object-fit:contain;overflow: hidden;"
+          alt="${alt}">
+      </picture>`;
+    }
+  );
   // Add a filter using the Config API
   eleventyConfig.addWatchTarget("./src/scss/");
   eleventyConfig.setBrowserSyncConfig({
